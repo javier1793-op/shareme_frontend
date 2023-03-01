f65e1873d6200430366badd385a504de309bd329
NicolasAyal <javier17utn@gmail.com>
2023-03-01 15:28:38 -0300

    login_google

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./Page/Home";
 import Login from "./Page/Login";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <GoogleOAuthProvider
+        clientId='40694529486-49gjidk0bmff287alh4lf7seu592mv5p.apps.googleusercontent.com'
+        >
+         <Routes>
         <Route path="login" element={<Login />} />
         <Route path="/*" element={<Home />} />
       </Routes>
+      </GoogleOAuthProvider>
+     
     </div>
   );
 }
--- src/Page/Login.jsx
+++ src/Page/Login.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import './../Css/login.scss'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom'
+import {FcGoogle} from "react-icons/fc"
+import chillhop from './../assets/chillhop.mp4'
+import logo from './../assets/logowhite.png'
 
 const Login = () => {
+
+const user=false
+
   return (
-    <div>Login</div>
+   <div className="container_login">
+    <div className="video_login">
+        <video src={chillhop}
+        type='video/mp4'
+        loop
+        controls={false}
+        muted
+        autoPlay
+        />
+        <div className="content_login">
+            <img src={logo} alt="logotipo_login" />
+            {user ? (
+             <div> logged ID</div>
+            ):
+            <GoogleLogin
+              onSuccess={(response)=>{
+                console.log(response)
+              }} 
+              onError={()=>{
+                console.log('Error')
+              }}
+            />
+            }
+        </div>
+    </div>
+   </div>
   )
 }
 
